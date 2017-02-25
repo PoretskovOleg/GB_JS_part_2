1f846982f5718161d0319cb83a1f3c11be5e1cf1
PorOleg <Porezkov@mail.ru>
2017-02-25 18:11:59 +0500

    JS part_2 HomeWork_4

--- JS_2_ДЗ_4_зад_2.php
+++ JS_2_ДЗ_4_зад_2.php
@@ -5,25 +5,30 @@
   <title> Урок 4 </title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <style>
-   
+    .green {
+      border: 2px solid green;
+    }
+    .red {
+      border: 2px solid red;
+    }
   </style>
 </head>
 <body>
   <form id="form">
     <label> Логин </label> <br>
-    <input type="text" name="username" placeholder="min 6 and max 100 simbols"> <br>
+    <input type="text" name="Username" placeholder="min 6 and max 100 simbols"> <br>
     <label> Пароль </label> <br>
-    <input type="password" name="password" placeholder="min 6 and max 100 simbols"> <br>
+    <input type="password" name="Password" placeholder="min 6 and max 100 simbols"> <br>
     <label> Электронная почта </label> <br>
-    <input type="email" name="email" placeholder="some@some.ru"> <br>
+    <input type="email" name="Email" placeholder="some@some.ru"> <br>
     <label> Пол </label> <br>
-    <input type="text" name="gender" placeholder="M"> <br>
+    <input type="text" name="Gender" placeholder="M"> <br>
     <label> Номер банковской карты </label> <br>
-    <input type="text" name="credit_card" placeholder="9872389-2424-234224-234"> <br>
+    <input type="text" name="Credit_Card" placeholder="9872389-2424-234224-234"> <br>
     <label> Дата рождения </label> <br>
-    <input type="text" name="birth" placeholder="yyyy-mm-dd"> <br>
+    <input type="text" name="Birth" placeholder="yyyy-mm-dd"> <br>
     <label> Краткая биография </label> <br>
-    <textarea name = "bio" rows="5" cols="20"> </textarea> <br>
+    <textarea name = "Bio" rows="5" cols="20"> </textarea> <br>
     <button id = "btn_form"> Отправить </button>
   </form>
   <script>
@@ -34,6 +39,7 @@
         var dataout = {};
         $('#form').find('input, textarea').each(function () {
           dataout[this.name] = $(this).val();
+          $(this).removeClass('red').removeClass('green');
         });
         $.ajax ({
           url: 'validator.php',
@@ -42,10 +48,15 @@
           dataType: 'json',
           success: function(data) {
             console.log(data.result ? 'Form is validate' : data.error);
+            $('#form').find('input, textarea').each(function () {
+              if (!data.result) { 
+                (this.name in data.error) ? $(this).addClass('red') : $(this).addClass('green');
+              } else $(this).addClass('green');
+            })
           },
           error: function(xhr, status, error) {
             console.log(xhr.responseText + '|\n' + status + '|\n' + error);
-          } 
+          }
         })
       })
     })(jQuery);
