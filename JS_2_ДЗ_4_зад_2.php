be22e0e639c8b7acff532f15152908cb9769ba04
PorOleg <Porezkov@mail.ru>
2017-02-26 10:34:25 +0500

    JS part_2 HomeWork_4

--- JS_2_ДЗ_4_зад_2.php
+++ JS_2_ДЗ_4_зад_2.php
@@ -31,35 +31,35 @@
     <textarea name = "Bio" rows="5" cols="20"> </textarea> <br>
     <button id = "btn_form"> Отправить </button>
   </form>
-  <script>
-    'use strict';
-    (function($) {
-      $('#btn_form').click(function(event) {
-        event.preventDefault();
-        var dataout = {};
+<script>
+'use strict';
+(function($) {
+  $('#btn_form').click(function(event) {
+    event.preventDefault();
+    var dataout = {};
+    $('#form').find('input, textarea').each(function () {
+      dataout[this.name] = $(this).val();
+      $(this).removeClass('red').removeClass('green');
+    });
+    $.ajax ({
+      url: 'validator.php',
+      type: 'POST',
+      data: dataout,
+      dataType: 'json',
+      success: function(data) {
+        console.log(data.result ? 'Form is validate' : data.error);
         $('#form').find('input, textarea').each(function () {
-          dataout[this.name] = $(this).val();
-          $(this).removeClass('red').removeClass('green');
-        });
-        $.ajax ({
-          url: 'validator.php',
-          type: 'POST',
-          data: dataout,
-          dataType: 'json',
-          success: function(data) {
-            console.log(data.result ? 'Form is validate' : data.error);
-            $('#form').find('input, textarea').each(function () {
-              if (!data.result) { 
-                (this.name in data.error) ? $(this).addClass('red') : $(this).addClass('green');
-              } else $(this).addClass('green');
-            })
-          },
-          error: function(xhr, status, error) {
-            console.log(xhr.responseText + '|\n' + status + '|\n' + error);
-          }
+          if (!data.result) { 
+            (this.name.replace('_',' ') in data.error) ? $(this).addClass('red') : $(this).addClass('green');
+          } else $(this).addClass('green');
         })
-      })
-    })(jQuery);
-  </script>
+      },
+      error: function(xhr, status, error) {
+        console.log(xhr.responseText + '|\n' + status + '|\n' + error);
+      }
+    })
+  })
+})(jQuery);
+</script>
 </body>
 </html>
